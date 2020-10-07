0a55d1ca96478cf4d92cafa0b7e1ca42981d8fea
Robin Malhotra <me@rmalhotra.com>
2020-10-07 01:09:25 +0100

    Add schemas
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

---
+++ sanity/schemas/pizza.js
@@ -0,0 +1,43 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  // computer name
+  name: 'Pizza',
+  // visible title
+  title: 'Pizzas',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of pizza in cents',
+      validation: (Rule) => Rule.min(1000),
+      // TODO: Add custom component
+    },
+  ],
+};
