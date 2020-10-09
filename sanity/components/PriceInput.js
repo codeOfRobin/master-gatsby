ec6cb070c3f5e068a66c14c0ca4d6c2df4b6d102
Robin Malhotra <me@rmalhotra.com>
2020-10-09 01:51:21 +0100

    Made values work better
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- sanity/components/PriceInput.js
+++ sanity/components/PriceInput.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event';
 
-export default function PriceInput(props) {
+function createPatchFrom(value) {
+  return PatchEvent.from(value === '' ? unset() : set(Number(value)));
+}
+
+const formatMoney = Intl.NumberFormat('en-GB', {
+  style: 'currency',
+  currency: 'GBP',
+}).format;
+
+function PriceInput({ type, value, onChange, forwardedRef }) {
   return (
     <div>
-      <h2>The Price</h2>
+      <h2>
+        {type.title} {value ? `- ${formatMoney(value / 100)}` : ''}
+      </h2>
+      <p>{type.description}</p>
+      <input
+        type={type.name}
+        value={value}
+        onChange={(event) => onChange(createPatchFrom(event.target.value))}
+        ref={forwardedRef}
+      />
     </div>
   );
 }
+
+// Sanity accessibility
+PriceInput.focus = function () {
+  this._inputElement.focus();
+};
+
+export default forwardRef((props, ref) => (
+  <PriceInput {...props} forwardedRef={ref} />
+));
