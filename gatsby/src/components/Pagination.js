1bf1c82baaa4e7a41ed2dd020c46d436cfa676ee
Robin Malhotra <me@rmalhotra.com>
2020-10-30 00:52:46 +0000

    Pagination Styles
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- gatsby/src/components/Pagination.js
+++ gatsby/src/components/Pagination.js
@@ -1,5 +1,30 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link } from "gatsby"; 
+import styled from 'styled-components';
+
+const PaginationStyles = styled.div`
+	display: flex;
+	align-content: center;
+	align-items: center;
+	justify-items: center
+	border: 1px solid var(--grey);
+	margin: 2rem 0;
+	border-radius: 5px;
+	& > * {
+		padding: 1rem;
+		flex: 1;
+		border-right: 1px solid var(--grey);
+		text-decoration: none;
+		&[aria-current], &.current {
+			color: var(--red);
+		}
+	}
+	
+	&[disabled] {
+		pointer-events: none;
+		color: var(--grey);
+	}
+`
 
 export default function Pagination({
   pageSize,
@@ -14,17 +39,17 @@ export default function Pagination({
   const hasNextPage = nextPage <= totalPages;
   const hasPrevPage = prevPage >= 1;
   return (
-    <div>
+    <PaginationStyles>
       <p>PAGINATION</p>
       <Link disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
         🔙 Prev
       </Link>
       {Array.from({ length: totalPages }).map((_, i) => (
-        <Link to={`${base}/${i > 0 ? i + 1 : ""}`}>{i + 1}</Link>
+        <Link className={currentPage === 1 && i === 0 ? 'current' : ""} to={`${base}/${i > 0 ? i + 1 : ""}`}>{i + 1}</Link>
       ))}
       <Link disabled={!hasNextPage} to={`${base}/${nextPage}`}>
         Next 🔜
       </Link>
-    </div>
+    </PaginationStyles>
   );
 }
