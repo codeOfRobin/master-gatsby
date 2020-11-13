2441683e84f6fc1384e626c286220ad8c30412bd
Robin Malhotra <me@rmalhotra.com>
2020-11-13 00:37:52 +0000

    Add order stuff
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,8 +1,10 @@
+import { graphql } from 'gatsby';
 import React, { useState } from 'react';
 import SEO from '../components/SEO';
 import useForm from '../utils/useForm';
 
-export default function OrderPage() {
+export default function OrderPage({ data }) {
+  const { pizzas } = data.pizzas.nodes;
   const { values, updateValue } = useForm({
     name: '',
     email: '',
@@ -22,9 +24,42 @@ export default function OrderPage() {
           />
 
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={updateValue}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Menu</legend>
+          {pizzas.map(pizza => (
+            <div key=
+          )}
         </fieldset>
       </form>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        price
+        image {
+          asset {
+            fluid(maxWidth: 100) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
