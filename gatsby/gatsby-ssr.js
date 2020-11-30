71e660714ad487c3fd86eddd3f95714ce0c1fdb8
Robin Malhotra <me@rmalhotra.com>
2020-11-30 00:35:20 +0000

    Add context stuff
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -1,5 +1,10 @@
 import Layout from './src/components/Layout';
+import { OrderProvider } from './src/components/OrderContext';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
+}
+
+export function wrapRootElement({ element }) {
+  return <OrderProvider>{element}</OrderProvider>;
 }
--- gatsby/src/components/OrderContext.js
+++ gatsby/src/components/OrderContext.js
@@ -3,9 +3,13 @@ import React, { useState } from 'react';
 const OrderContext = React.createContext();
 
 export function OrderProvider({ children }) {
-  const [order, setOrder] = useState([':poop:']);
+  const [order, setOrder] = useState([]);
 
-  return <OrderContext.Provider>{children}</OrderContext.Provider>;
+  return (
+    <OrderContext.Provider value={[order, setOrder]}>
+      {children}
+    </OrderContext.Provider>
+  );
 }
 
 export default OrderContext;
